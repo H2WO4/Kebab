824a40a7d06450d6b98a4f84f5451c842b31f686
Tom <Tom602001@live.fr>
2022-09-29 15:50:34 +0200

    Update Program.cs
    Choix

--- Kebab/Program.cs
+++ Kebab/Program.cs
@@ -18,18 +18,77 @@ public class Program
 		return 0;
 	}
 
-	static void AskRecipe()
+	static Models.Kebab NewKebab()
 	{
 		Console.WriteLine("Que voulez vous dans le kebab ?");
 
-		int choixIngre = -1;
+        var kebab = new Models.Kebab();
+        int choixIngre = -1;
 
-		Console.WriteLine("1 : Viande, 2 : Crudité");
-		var choix = Console.ReadLine();
-		if (int.TryParse(choix, out choixIngre))
+		while (true)
 		{
+			Console.WriteLine("1 : Viande, 2 : Crudité\n0 : Finir creation du kebab");
+			var choix = Console.ReadLine();
 
-			// TO REDO
+			if (int.TryParse(choix, out choixIngre))
+			{
+				if (choixIngre == 0)
+					break;
+				if (choixIngre == 1)
+				{
+					var viande = AskViandes();
+					if (viande != null)
+						kebab.AddViande(viande.Value);
+					else
+						Console.WriteLine("Mauvais choix");
+
+				}
+				else if (choixIngre == 2)
+				{
+					var crudite = AskCrudites();
+					if (crudite != null)
+						kebab.AddCrudite(crudite.Value);
+					else
+						Console.WriteLine("Mauvais choix");
+				}
+				else
+				{
+					Console.WriteLine("Mauvais choix");
+				}
+			}
 		}
-	}
+
+		return kebab;
+
+    }
+
+	static Crudites? AskCrudites()
+    {
+        foreach (Crudites crudite in Enum.GetValues(typeof(Crudites)))
+        {
+            Console.WriteLine($"{(int)crudite} : {crudite}");
+        }
+
+		var choix = Console.ReadLine();
+
+		if (int.TryParse(choix, out var crud))
+			return (Crudites)crud;
+		else
+			return null;
+    }
+
+    static Viandes? AskViandes()
+    {
+        foreach (Viandes viande in Enum.GetValues(typeof(Viandes)))
+        {
+            Console.WriteLine($"{(int)viande} : {viande}");
+        }
+
+        var choix = Console.ReadLine();
+
+        if (int.TryParse(choix, out var crud))
+            return (Viandes)crud;
+        else
+            return null;
+    }
 }
